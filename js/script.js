83f7fc8b33999fe1012dff6d9a468a696c60b33b
facoulon <f.coulon48@laposte.net>
2018-06-13 12:23:53 +0200

    Ex End

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var shift = 5;
+var word = "bonjour";
+var wordEncode = "";
 
-var textBrut = document.getElementById('textBrut');
-var textChiffrer = document.getElementById('textChiffrer');
-
-var shift = document.getElementsByTagName('input');
+for (var i = 0; i < word.length; i++) {
+   var LetterIndex = alpha.indexOf(word[i])
+   var newLetterIndex = LetterIndex + shift;
+   newLetterIndex %= 26;
+   var newLetter = alpha[newLetterIndex];
+   wordEncode += newLetter;
+}
+console.log(word , wordEncode);
